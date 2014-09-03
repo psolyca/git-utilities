753997e72bb04ddb483e33440a4fca553472b58d
goncalopp <goncalopp@dell0123041.collab.pt>
2014-09-03 18:10:17 +0100

    maintaining relative branch tree structure on rebase

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ git-utilities
 
 [git-multi-rebase]
 
-A script to rebase multiple branches onto a single base.  This script will iteratively rebase each branch onto the 'newbase'.  If confilicts arise the script will start a new shell instance, allowing the user to resolve the conflicts, or abort the conflicting rebase (using git rebase --abort).  Once the conflicts have been resolved, the user must execute git rebase --continue, and then exit the shell.  Once the shell exits the rest of the multi-rebase process will continue.  
+A script to rebase multiple branches onto a single base, preserving relations between branches (see [this stackoverflow question](https://stackoverflow.com/questions/23386318/how-can-i-rebase-multiple-branches-at-once) for an example).
+
+This script will iteratively rebase each branch onto the 'newbase'.  
+
+If confilicts arise the script will start a new shell instance, allowing the user to resolve the conflicts, or abort the conflicting rebase (using git rebase --abort). Once the conflicts have been resolved, the user must execute git rebase --continue, and then exit the shell. Once the shell exits the rest of the multi-rebase process will continue.  
 
 usage: git-multi-rebase [options] newbase branch0 [branch1 ... branchN]
 
--- git_python.py
+++ git_python.py
@@ -16,54 +16,51 @@ def is_dirty():
    proc = subprocess.Popen(['git', 'diff', '--shortstat'], 
                            stdout=subprocess.PIPE)
    if proc.stdout.read().strip() != "":
-      return "dirty"
+      return True
    else:
-      return "clean"
+      return False
 
 def checkout(branch):
-   proc = subprocess.Popen(['git', 'checkout', branch],
-                          stderr=subprocess.STDOUT, stdout=subprocess.PIPE) 
-   rc = proc.wait()
-   if rc != 0:
-      print proc.stdout
-      print "git checkout : unable to checkout branch " + branch
-      return -1
-   return 0
+   if branch==cur_branch():
+      return
+   proc = subprocess.check_call(['git', 'checkout', branch]) 
 
-def stash():
-   proc = subprocess.Popen(['git', 'stash'], 
-                          stderr=subprocess.STDOUT, stdout=subprocess.PIPE) 
-   rc = proc.wait()
-   if rc != 0:
-      print proc.stdout.read()
-      print "git stash: unable to stash local modifications"
-      return -1
-   return 0
+def merge_base( branches ):
+   '''returns the most recent common ancestor between branches'''
+   result= subprocess.check_output(['git', 'merge-base']+branches)
+   assert len(result.splitlines()[0])==40 #SHA1 hash size plus newline
+   return result.splitlines()[0]
 
-def stash_pop():
-   proc = subprocess.Popen(['git', 'stash', 'pop'], 
-                          stderr=subprocess.STDOUT, stdout=subprocess.PIPE) 
-   rc = proc.wait()
-   if rc != 0:
-      print proc.stdout.read()
-      print "git stash pop: unable to pop stash"
-      return -1
-   return 0
+def merge_base_from_commits_list( l1, l2 ):
+   '''returns the most recent common ancestor between lists of commits.
+   List MUST BE ORDERED FROM OLD TO RECENT'''
+   assert l1[0]==l2[0]
+   last= l1[0]
+   i=0
+   for c1,c2 in zip(l1,l2):
+      if c1!=c2:
+         return i-1, last #index and hash
+      last= c1
+      i+=1
 
-
-def svn_rebase():
-   proc = subprocess.Popen(['git', 'svn', 'rebase'], 
-                          stderr=subprocess.STDOUT, stdout=subprocess.PIPE) 
-   rc = proc.wait()
-   if rc != 0:
-      print proc.stdout.read()
-      print "svn rebase: execute 'git rebase --continue|--abort', then exit shell to continue the svn rebase process"
-      subprocess.call(['$SHELL'], shell=True)
-   return 0
+def commit_author_date( commit ):
+   return subprocess.check_output(["git", "log", "-1", '--format=%ad', commit] )
+   
+def commits_between( old_branch, new_branch, rev= False ):
+   '''returns the hashes of the commits between two branches,
+   ordered from new to old (unless rev)'''
+   result = subprocess.check_output(['git', 'log', '--format=%H', old_branch+"^.."+new_branch]) 
+   result= result.splitlines()
+   assert all([len(x)==40 for x in result]) #SHA1 hash size
+   if rev:
+      result= list(reversed(result))
+   return result
  
-def rebase(base, branch, flags=[]):
+def rebase(base, branch, onto=None, flags=[]):
    args = ['git', 'rebase']
-   args = args + flags
+   if onto:
+      args+= ['--onto', onto]
+   args += flags
    args.append(base)
    args.append(branch)
 
@@ -75,9 +72,14 @@ def rebase(base, branch, flags=[]):
       subprocess.call(['$SHELL'], shell=True)
    return 0
 
+def create_branch(branchname):
+   subprocess.check_call(['git', 'branch', branchname])
 
+def delete_branch(branchname, force=False):
+   delete_flag= "-D" if force else "-d"
+   subprocess.check_call(['git', 'branch', delete_flag, branchname])
 
-
-
+def rename_branch(branch, newbranch):
+   subprocess.check_call(['git', 'branch', "-m", branch, newbranch])
  
 
